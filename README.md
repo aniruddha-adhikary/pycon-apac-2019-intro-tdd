05c878fa5b9751c899fc0560cd2fa1ff56f7a50b
Aniruddha Adhikary <aniruddha@telenordigital.com>
2019-02-23 09:56:09 +0800

    add step 1: naive testing

--- README.md
+++ README.md
@@ -16,3 +16,27 @@ PyCon APAC 2019 (Manila, PH)
 | 6   | `step-7` | Mocking the World Beyond           |
 | 8   | `step-8` | Refactoring for Testing            |
 | 9   | `step-9` | Measuring Test Coverage            |
+
+
+## Business Logic
+
+https://en.wikipedia.org/wiki/Payment_card_number#Issuer_identification_number_.28IIN.29
+
+| Issuer           | IIN ranges |
+| ---------------- | ---------- |
+| American Express | 34, 37     |
+| MasterCard       | 51–55      |
+| Visa             | 4          |
+
+The card number has to start with the mentioned IIN range(s).
+
+## Running Tests
+
+```
+$ python card_validator/issuers.py
+```
+
+## Tasks
+
+1. Fix the `get_issuer` function in `card_validator/issuers.py` to fix a bug.
+2. Add a test case for testing American Express cards.
---
+++ card_validator/issuers.py
@@ -0,0 +1,40 @@
+"""
+Business Logic
+
+https://en.wikipedia.org/wiki/Payment_card_number#Issuer_identification_number_.28IIN.29
+
+| Issuer           | IIN ranges |
+| ---------------- | ---------- |
+| American Express | 34, 37     |
+| MasterCard       | 51–55      |
+| Visa             | 4          |
+"""
+
+
+def get_issuer(number: str):
+    if number.startswith('4'):
+        return 'MasterCard'
+
+    second_digit = number[1]
+
+    if number.startswith('3') and second_digit in ('4', '7'):
+        return "American Express"
+
+    if number.startswith('5') and second_digit in ('1', '2', '3', '4', '5'):
+        return "Visa"
+
+    raise ValueError("Unknown Card Type")
+
+
+if __name__ == '__main__':
+
+    # Fix the logic for detecting Visa and MasterCard cards in the `get_issuer` function above.
+
+    assert get_issuer('4862876677853409') == 'Visa'
+    print("Visa test passed!")
+    assert get_issuer('5462876677853409') == 'MasterCard'
+    print("MasterCard test passed!")
+
+    # Replace the following assertion with a test for American Express Cards
+    assert False
+    print("All tests passed!")
